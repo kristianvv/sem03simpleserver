c471ade0e9b34ec2b6040d288ae4ebc40fa7073e
kristianvv <112623460+kristianvv@users.noreply.github.com>
2023-04-24 15:11:25 +0200

    Update tcp_server.go

--- tcp_server.go
+++ tcp_server.go
@@ -16,7 +16,7 @@ import (
 func main() {
 	var wg sync.WaitGroup
 
-	server, err := net.Listen("tcp", "172.17.0.2:8300")
+	server, err := net.Listen("tcp", "172.17.0.2:8082")
 	if err != nil {
 		log.Fatal(err)
 	}
